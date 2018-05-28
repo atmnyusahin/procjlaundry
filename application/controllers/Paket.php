16637db3388677dd8b16f81284edebdcf757105a
shofiuliqbal <syqibal@gmail.com>
2018-05-28 23:54:48 +0700

    pakeett

--- application/controllers/Paket.php
+++ application/controllers/Paket.php
@@ -3,23 +3,42 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Paket extends US_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
+	function __construct(){
+        parent::__construct();
+        $this->load->model('M_paket');
+    }
+
 	public function index()
 	{
 		$this->load->view('paket');
+	}
+
+	function ajaxTable()
+	{
+		$list = $this->M_paket->tampil();
+        $data = array();
+        $no = $_POST['start'];
+        foreach ($list as $key) {
+            $no++;
+            $row = array();
+            $row[] = $key->id_satuan;
+            $row[] = $key->nama;
+            $row[] = $key->harga;
+            $row[] = $key->jangka_waktu;
+            
+            $row[] = '
+            <a href="#" data-toggle="tooltip" data-original-title="Edit" onclick="show_edit('.$key->id_paket.')"> <i class="fa fa-pencil text-inverse m-r-10"></i> </a>
+            <a href="javascriptvoid:" data-toggle="tooltip" data-original-title="Hapus" onclick="hapus('.$key->id_paket.')"> <i class="fa fa-close text-danger"></i> </a>
+
+            ';
+            $data[] = $row;
+            
+        }
+        $output = array("draw" => $_POST['draw'],
+                        "recordsTotal" => $this->M_paket->count_all(),
+                        "recordsFiltered" => $this->M_paket->count_filtered(),
+                        "data" => $data);
+
+        echo json_encode($output);
 	}
 }
